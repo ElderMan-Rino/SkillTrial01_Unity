2d4db00a5d515318cbdca949015b80dceec627ed
DESKTOP-FUOEQT8\ovate <ovate9006@gmail.com>
2025-06-30 00:54:57 +0900

    ElderMan-Rino로 업로드

---
+++ Assets/Scripts/Unity/Bootstrappers/Application/Bootstrapper.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Elder.Unity.Bootstrappers.Application
+{
+    public static class Bootstrapper
+    {
+        private const string GameObjectName = "[MainFramework]";
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+        public static void Entry()
+        {
+            CreateMainFramework();
+        }
+        private static void CreateMainFramework()
+        {
+            if (GameObject.Find(GameObjectName))
+                return;
+
+            var go = new GameObject(GameObjectName);
+           
+        }
+    }
+}
